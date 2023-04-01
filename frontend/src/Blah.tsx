53b3f18f469f543a38aaef340b6421fbef86dc8b
Nathan Johnson <nj236@byu.edu>
2023-03-31 20:50:13 -0600

    Fixed Prettier FR FR

---
+++ frontend/src/Blah.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TopBanner () {
+    return (
+        <React.Fragment>
+            <img src="./logo192.png" alt="Company Logo"/>
+            <h1>Nate's React Site</h1>
+        </React.Fragment>
+    );
+}
+
+export default TopBanner;
